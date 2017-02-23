8878d37e0fcd90d06f0202196886b0e8f491fbdc
Brent Perry <burentoperii@gmail.com>
2017-02-23 15:05:18 -0600

    add exercises for 1.4.4

--- ch1/printRange.cpp
+++ ch1/printRange.cpp
@@ -3,12 +3,19 @@
 int main()
 {
   std::cout << "Please enter two integers: ";
-  int min = 0, max = 0;
-  std::cin >> min >> max;
+  int num1 = 0, num2 = 0;
+  std::cin >> num1 >> num2;
   std::cout << "Here are the numbers in that range:" << std::endl;
-  while (min <= max) {
-    std::cout << min << std::endl;
-    ++min;
+  if (num1 >= num2) {
+    while (num2 <= num1) {
+      std::cout << num2 << std::endl;
+      ++num2;
+    }
+  } else {
+    while (num1 < num2) {
+      std::cout << num1 << std::endl;
+      ++num1;
+    }
   }
   return 0;
 }

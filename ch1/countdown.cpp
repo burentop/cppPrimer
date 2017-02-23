61b396b246d33c855bea4bf2b324127e3c713c98
Brent Perry <burentoperii@gmail.com>
2017-02-23 13:34:09 -0600

    add exercises for 1.4.2

--- ch1/countdown.cpp
+++ ch1/countdown.cpp
@@ -2,10 +2,8 @@
 
 int main()
 {
-  int val = 10;
-  while (val >= 0) {
-    std::cout << val << std::endl;
-    --val;
+  for (int i = 10; i >= 0; --i) {
+    std::cout << i << std::endl;
   }
   return 0;
 }
--- ch1/whileSum.cpp
+++ ch1/whileSum.cpp
@@ -4,9 +4,8 @@ int main()
 {
   int sum = 0, val = 50;
   // keep executing the while as long as val is less than or equal to 100
-  while (val <= 100) {
+  for (int i = 50; i <= 100; ++i) {
     sum += val; // assigns sum + val to sum
-    ++val;      // add 1 to val
   }
   std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
   return 0;
